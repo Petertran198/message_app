e8300b00f897d5690032deade9ef3e4068cbc0c6
petertran198 <petertran198@gmail.com>
2019-01-26 19:45:45 -0500

    Added User model, bcrypt gem, and seeds for user attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "peter", password: "123456")
+User.create(username: "joanne", password: "123456")
+User.create(username: "pete", password: "123456")
+User.create(username: "bob", password: "123456")
+User.create(username: "dan", password: "123456")
