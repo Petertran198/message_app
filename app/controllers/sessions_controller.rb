fde21f4481257d777be8a777d62622bc6bd09952
petertran198 <petertran198@gmail.com>
2019-01-26 22:59:38 -0500

    Implemented dynamic flash messaging in  messages partial

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,36 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
-def new
+  def new
 
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong with your infomation"
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = " You have logged out"
+    redirect_to login_path
+  end
+
+private
+
+def logged_in_redirect
+  if logged_in?
+    flash[:error] = "You already logged in"
+    redirect_to root_path
+  end  
 end
-
 
 end
